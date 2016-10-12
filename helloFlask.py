70880b42971b2241f46d81c7a59a396a200cd432
Jodie Gardiner <jodiegardiner@outlook.com>
2016-10-12 16:34:09 +0100

    styling

--- helloFlask.py
+++ helloFlask.py
@@ -5,19 +5,26 @@ app = Flask(__name__)
 heroes = [{
     'name': 'Luke Skywalker',
     'home': 'Tatooine',
-    'weapon': 'Lightsaber'
+    'weapon': 'Lightsaber',
+    'url':'http://starwars.wikia.com/wiki/Luke_Skywalker',
+    'img':'http://vignette3.wikia.nocookie.net/starwars/images/1/15/Luke_Skywalker_Ep_7_SWCT.png'
 },
 
     {
         'name': 'Chewbacca',
         'home': 'Kashyyyk',
-        'weapon': 'Bowcaster'
+        'weapon': 'Bowcaster',
+        'url':'http://starwars.wikia.com/wiki/Chewbacca',
+        'img':'http://vignette4.wikia.nocookie.net/starwars/images/4/4f/Chewbacca-TFA.png'
     },
 
     {
         'name': 'Han Solo',
         'home': 'Corellia',
-        'weapon': 'DL-44 Heavy Blaster Pistol'
+        'weapon': 'DL-44 Heavy Blaster Pistol',
+        'url': 'http://starwars.wikia.com/wiki/Han_solo',
+        'img':'http://vignette2.wikia.nocookie.net/starwars/images/e/e2/TFAHanSolo.png'
+
     }]
 
 @app.route('/')
